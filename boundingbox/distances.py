374ec26616222fe3d1bca9f329443f8974dee3bb
Nick Halmagyi <nhalmagyi@gmail.com>
2019-05-09 21:19:41 +1000

    removed computation of box length

--- boundingbox/distances.py
+++ boundingbox/distances.py
@@ -9,33 +9,6 @@ from boundingbox.validations.numbers import validate_strictly_positive_integer
 from boundingbox.validations.coordinates import validate_latlons_degrees
 
 
-def closest_points_lat_lon(source, targets, N):
-    """
-    :param source: lat-lon tuple
-    :param lat_lons: iterable of lat-lon tuples
-    :param N: integer greater than zero
-    :return: A pair of iterables of lat-lon tuples.
-    closest_lats is the closest N elements of lat_lons to source measured by latitude
-    closest_lons is the closest N elements of lat_lons to source measured by longitude
-    """
-    lat_lon_diffs = np.abs(np.array(targets) - np.array(source))
-    lat_lons = np.concatenate([targets, lat_lon_diffs], axis=1)
-    closest_lats = lat_lons[lat_lons[:, 2].argsort()][:,:2][:N]
-    closest_lons = lat_lons[lat_lons[:, 3].argsort()][:,:2][:N]
-    return closest_lats, closest_lons
-
-
-def make_bounding_box_length(source, lat_lons, N=1):
-    len_lat_lons = len(lat_lons)
-    if N > len_lat_lons:
-        N = len_lat_lons
-    closest_points = closest_points_lat_lon(source, lat_lons, N)
-    distance_lat = haversine(source, closest_points[0][-1])
-    distance_lon = haversine(source, closest_points[1][-1])
-    bbox_length = (distance_lat + distance_lon)/2
-    return bbox_length
-
-
 def get_points_within_distance(source, targets, length):
     """
     It is possible for a point to be within the bbox but further than length from source.
@@ -77,9 +50,6 @@ def get_closest_points(source_degrees, targets, N, length=None, validate=False):
     if N > len(targets):
         # should just return all targets with distance and sorted by distance
         N = len(targets)
-
-    if length == None:
-        length = make_bounding_box_length(source_degrees, targets, N)
 
     boundingbox = BoundingBox(source_degrees, length)
     targets_filtered = boundingbox.filter_targets_in_bboxs(targets, boundingbox.bbox)
